59159153afb4cb5dd17441278288c1e5b1bd488a
Treyvon Whitaker <treyvonwhitaker2025@gmail.com>
2025-06-06 16:25:31 -0500

    feat(treyvon): add active route styles to sidenav

--- TODO.md
+++ TODO.md
@@ -1,3 +1,3 @@
 1. Add version service to validate feature that a version dependent
 2. Change selector prefix When moving components into separate librarys
-3. Add active route indicator to sidenav
+3. Fix transition for sidenav
--- src/main/main.component.ts
+++ src/main/main.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { NavigationComponent } from './module/navigation/navigation.component';
 import { MatIconModule } from '@angular/material/icon';
 import { SharedService } from './module/shared/shared.service';
 import { MatButtonModule } from '@angular/material/button';
 
+type NavMenuItem = { link: string, icon: string, label: string };
+
 @Component({
     selector: 'main-root',
-    imports: [RouterOutlet, RouterLink, NavigationComponent, MatButtonModule, MatIconModule],
+    imports: [RouterOutlet, RouterLink, RouterLinkActive, NavigationComponent, MatButtonModule, MatIconModule],
     templateUrl: './main.component.html',
     styleUrl: './main.component.scss'
 })
@@ -16,6 +18,18 @@ export class MainComponent {
     protected iconName = 'shell';
 
     protected fontSet: string;
+    protected navMenuItems: NavMenuItem[] = [
+        {
+            link: 'description',
+            icon: 'description',
+            label: 'Description'
+        },
+        {
+            link: 'playground',
+            icon: 'code',
+            label: 'Playground'
+        }
+    ]
 
     constructor(
         public shared: SharedService
